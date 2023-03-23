73d2bf74940f9f1654677c85937edc850c9e0447
mores-hitt <fernando.sanabria@alumnos.ucn.cl>
2023-03-23 04:01:15 -0300

    Fix: fixed bug

--- frontend/client/src/features/carrerasData.js
+++ frontend/client/src/features/carrerasData.js
@@ -3,7 +3,7 @@ import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
 export const carrerasApi = createApi({
     reducerPath: "carrerasApi",
-    baseQuery: fetchBaseQuery({baseUrl: "142.93.1.236"}),
+    baseQuery: fetchBaseQuery({baseUrl: "http://142.93.1.236/"}),
     endpoints: (builder) => ({
         getCarreras: builder.query({
             query: () => "/api/carreras",
